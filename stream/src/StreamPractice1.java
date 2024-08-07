f8941ce205ed7f36ce47d040f2f2178cd9df9175
omelet3032 <omelet3032@gmail.com>
2024-08-07 16:13:39 +0900

    스트림 문제 분류

--- stream/src/StreamPractice1.java
+++ stream/src/StreamPractice1.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class StreamPractice {
+public class StreamPractice1 {
     public static void main(String[] args) {
         
         /*
---
+++ stream/src/StreamPractice2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamPractice2 {
+    public static void main(String[] args) {
+        
+        /*
+         * 다음 작업을 스트림을 사용하여 수행하세요:
+         * 2. 각 도시별 평균 나이를 구하시오.
+         */
+
+        List<Person> people = Arrays.asList(
+                new Person("Alice", 25, "New York"),
+                new Person("Bob", 30, "London"),
+                new Person("Charlie", 35, "New York"),
+                new Person("David", 40, "London"),
+                new Person("Eve", 45, "Paris"));
+        
+    }
+}
+
